a288a0da5fd2c5203bd70ee31867875f150bb8c8
Samantha Copland <sam.copland1992@gmail.com>
2020-05-11 14:42:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function shout (string) {
 function whisper (string) {
   return string.toLowerCase()
 }
-function logShout (string) {console.log (string.toUpperCase)}
+function logShout (string) {console.log (string.toUpperCase);}
