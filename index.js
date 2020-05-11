04ac7a03b989bdf82ed02085d563b679eb9a715d
Samantha Copland <sam.copland1992@gmail.com>
2020-05-11 14:51:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function whisper (string) {
 }
 function logShout (string) {console.log (string.toUpperCase());}
 function logWhisper (string) {console.log (string.toLowerCase ());}
+function sayHiToGrandma (string) {
+  var lowercase = "I can't hear you!";
+  var uppercase = "YES INDEED!";
+  var love = "I love you, too.";
+  if (string.toLowerCase (string) === string) {return lowercase;}
+  else if (string.toUpperCase (string) === string) {return uppercase;}
+  else if ("I love you, Grandma." === string) {return love}
+}
